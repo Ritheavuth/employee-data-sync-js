54cc0d440d637c2a0e39b443b6fd6244caced11c
Ritheavuth <hinchanritheavuth20@kit.edu.kh>
2023-06-25 01:02:38 +0700

    Finish api for mysql

--- src/config/db.js
+++
@@ -1,9 +0,0 @@
-const dbConfig = {
-  host: "employee-db.cb1nrnmeyfto.us-east-1.rds.amazonaws.com",
-  port: 5432,
-  database: "employee_db",
-  user: "paylessgate",
-  password: "admin123",
-};
-
-module.exports = { dbConfig };
